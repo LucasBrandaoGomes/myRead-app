0e4d32cfe4c1cfa595a1e08f2bf12a970876a78d
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-01 16:34:54 -0300

    feat: create calendar structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,24 @@
-export default function Main(){
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import styled from 'styled-components'
+
+export default class DemoApp extends React.Component {
+  render() {
     return (
-        <>
-        <h1>CALENDARIO</h1> 
-        </>
+        <CalendarContainer>
+            <FullCalendar
+                plugins={[ dayGridPlugin ]}
+                initialView="dayGridMonth"
+            />
+        </CalendarContainer>
     )
+  }
 }
+
+const CalendarContainer = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    
+`
