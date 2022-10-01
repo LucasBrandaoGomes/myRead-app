b6140935542a4da3bde9d285a8a4893591482fa3
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2022-10-01 13:42:43 -0300

    feat: import signup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
-export default function App() {
+import  {  BrowserRouter ,  Routes ,  Route  }  from  'react-router-dom' ;
+import  {  useState  }  from  'react' ;
+import SignUp from './SignUp.js'
+import Context from "../contexts/Context.js";
 
-  return (
-    <>
-        <h1>Teste</h1>
-    </>
-  )
+
+export default function App(){
+    
+    const [infoLogin, setInfoLogin] = useState();
+    
+    return(
+        <Context.Provider value = {{infoLogin , setInfoLogin }}>
+            <BrowserRouter>
+              <Routes>
+                  <Route path="/sign-up" element={<SignUp />}/> 
+              </Routes>    
+            </BrowserRouter>
+        </Context.Provider>
+    )
 }
